714a7ed79274482e933f938e24a0e885233f20c6
William Garzon <167917603+RecHashhh@users.noreply.github.com>
2026-04-18 03:17:40 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,25 +7,30 @@ import { msalInstance } from './authConfig'
 import App from './App'
 import './index.css'
 
-msalInstance.initialize().then(() => {
-  const accounts = msalInstance.getAllAccounts()
-  if (accounts.length > 0) {
-    msalInstance.setActiveAccount(accounts[0])
+await msalInstance.initialize()
+
+const redirectResult = await msalInstance.handleRedirectPromise()
+if (redirectResult?.account) {
+  msalInstance.setActiveAccount(redirectResult.account)
+}
+
+const accounts = msalInstance.getAllAccounts()
+if (!msalInstance.getActiveAccount() && accounts.length > 0) {
+  msalInstance.setActiveAccount(accounts[0])
+}
+
+msalInstance.addEventCallback((event) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+    msalInstance.setActiveAccount(event.payload.account)
   }
-
-  msalInstance.addEventCallback((event) => {
-    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
-      msalInstance.setActiveAccount(event.payload.account)
-    }
-  })
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <MsalProvider instance={msalInstance}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </MsalProvider>
-    </React.StrictMode>
-  )
 })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MsalProvider>
+  </React.StrictMode>
+)
